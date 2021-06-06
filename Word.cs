6d05da2ea65ef5f436d6ab5a5556ae6e44bb9e50
stbrumbaugh <56395609+stbrumbaugh@users.noreply.github.com>
2021-06-06 16:49:11 -0700

    Update Word.cs
    deleted old method

--- Word.cs
+++ Word.cs
@@ -24,56 +24,6 @@ namespace Thinker
             //AlphaIndex = CalcAlphaIndex();
         }
 
-        /*private int[] CalcAlphaIndex()
-        {
-            int[] startIndex = new int[3];
-
-            int firstIndex = alphabet.IndexOf(Spelling[0]);
-            // this is the case that the first letter isn't in the english alphabet
-            if (firstIndex == -1)
-            {
-                firstIndex = 26;
-            }
-            if (Spelling.Length == 1)
-            {
-                startIndex[0] = firstIndex;
-                startIndex[1] = 0;
-                startIndex[2] = 0;
-                return startIndex;
-            }
-            int secondIndex;
-            secondIndex = alphabet.IndexOf(Spelling[1]);
-            // the case that the second letter isnt in the english alphabet
-            if (secondIndex == -1)
-            {
-                secondIndex = 27;
-            }
-            else
-                secondIndex++;
-            if (Spelling.Length == 2)
-            {
-                startIndex[0] = firstIndex;
-                startIndex[1] = secondIndex;
-                startIndex[2] = 0;
-                return startIndex;
-            }
-            int thirdIndex;
-            thirdIndex = alphabet.IndexOf(Spelling[2]);
-            // the case that the third letter isnt in the english alphabet
-            if (thirdIndex == -1)
-            {
-                thirdIndex = 27;
-            }
-            else
-                thirdIndex++;
-            // gather and return info
-            startIndex[0] = firstIndex;
-            startIndex[1] = secondIndex;
-            startIndex[2] = thirdIndex;
-
-            return startIndex;
-        }*/
-
         public override string ToString()
         {
             return Spelling + ":" + NumberOfWord;
